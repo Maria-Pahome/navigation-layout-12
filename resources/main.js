3cc14bb24fd85a5a0b5437bed3e68b66f97ab27d
Maria-Pahome <70057309+Maria-Pahome@users.noreply.github.com>
2020-10-22 20:09:28 +0300

    4/5 pages created
     Js needs work still

---
+++ resources/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const menuItems = document.getElementsByClassName ("menu-section");
+    console.log ('menuItems', menuItems)
+
+    for (let i = 0; i < menuItems.length; i++) {
+        const menuItem = menuItems[i];
+        
+        menuItem.addEventListener('click', function() {
+            for (let j = 0; j < menuItems.length; j++) {
+                if(menuItems[j].classList.contains('selected')){
+                    menuItems[j].classList.remove("selected");
+                } else {
+                    continue;
+                }
+            }
+            menuItem.classList.add("selected");
+
+        const dataContent = menuItem.dataset.content;
+        console.log('dataContent', dataContent)
+        const contentElements = document.getElementsByClassName("content");
+        for(let k = 0; k < contentElements.length; k++) {
+            if(contentElements[k].classList.contains('hidden')) {
+                continue;
+            } else {
+                contentElements[k].classList.add("hidden");
+            }
+        }
+        document.getElementsByClassName(dataContent)[0].classList.remove("hidden");
+        document.getElementsByClassName(dataContent)[1].classList.remove("hidden");
+        document.getElementsByClassName(dataContent)[2].classList.remove("hidden");
+    });
+}
+});
